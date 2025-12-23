20dd532668f6abe1ace423a65650f554ca8bc986
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2025-12-23 18:15:21 +0000

    Remove extra newline before closing form tag

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -59,7 +59,6 @@ export default function cadastro() {
                                 Criar conta
                             </button>
                         </Link>
-
                     </form>
 
                 </div>
