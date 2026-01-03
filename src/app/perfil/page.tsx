8bf89248b01a810931d71aa7c54a338e212ee3d3
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2026-01-03 14:46:50 +0000

    Add layout for Perfil page

--- src/app/perfil/page.tsx
+++ src/app/perfil/page.tsx
@@ -1,6 +1,7 @@
 
 export default function Perfil(){
     return(
+        
         <div className="flex items-center justify-center h-screen">
             <h1 className="text-3xl">Perfil</h1>
         </div>
