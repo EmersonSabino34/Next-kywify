033e65e37b2dc95a3e948c1494cf69a46211b261
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2026-01-03 14:46:35 +0000

    Add state for email and password in login component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function login() {
+ 
  const [email, setEmail] = useState("")
  const [password, setPassword] = useState("")
 
