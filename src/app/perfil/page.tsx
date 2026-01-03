c31501384a535a4633539ad50193e6097cf3d2af
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2026-01-03 14:46:58 +0000

    Fix indentation in Perfil component return statement

--- src/app/perfil/page.tsx
+++ src/app/perfil/page.tsx
@@ -1,7 +1,6 @@
 
 export default function Perfil(){
-    return(
-        
+    return(  
         <div className="flex items-center justify-center h-screen">
             <h1 className="text-3xl">Perfil</h1>
         </div>
