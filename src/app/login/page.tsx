41cbc824d2d1d97f87cfc42f2cc64278b785a735
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2026-01-12 16:31:14 +0000

    Refactor login component state initialization

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function login() {
- 
  const [email, setEmail] = useState("")
  const [password, setPassword] = useState("")
 
